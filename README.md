dabdea9fe5853703895650594e6342d0cd864e54
Pelly Ryu <pelly.ryu@gmail.com>
2017-10-24 00:14:56 +0900

    feat: route /open

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # nocky-remote-door-lock
  $100 business project of university lecture
+
+# How to Use
+- `/open` : `true` or `false`
+- `/set-open` : 다음 번 `/open` 요청의 response를 open으로 바꿈
--- server.js
+++ server.js
@@ -2,11 +2,29 @@ const express = require('express')
 const app = express()
 
 // noinspection JSUnresolvedFunction
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.send('hello world')
 })
 
+let open = false
+
+// noinspection JSUnresolvedFunction
+app.get('/open', (req, res) => {
+  if (open) {
+    open = false
+    res.send(true)
+  } else {
+    res.send(false)
+  }
+})
+
+// noinspection JSUnresolvedFunction
+app.get('/set-open', (req, res) => {
+  open = true
+  res.send('done')
+})
+
 const PORT = 8080
-app.listen(PORT, function () {
+app.listen(PORT, () => {
   console.log('Example app listening on port ' + PORT)
 })
